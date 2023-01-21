edcbda0f33f58e8ac4ff7b6a791445232e588c0e
Zelius5 <zuky2435@gmail.com>
2023-01-20 22:36:57 -0500

    Initial commit for public repo

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 "use strict";
 "use strict";
 (function(){
+
   function start()
   {
     console.log("App Started!");
@@ -22,7 +23,23 @@
         break;
     }
   }
+  let MainContent = document.getElementsByName("main")[0];
+  let Document = document.body;
+  let MainParagraph = document.createElement("p")
+  MainParagraph.setAttribute("id", "MainParagraph")
+  MainParagraph.setAttribute("class","mt-3")
+  MainParagraph.textContent = "This is the main paragraph!";
+  MainContent.appendChild(MainParagraph);
   window.addEventListener("load", start)
+  let FirstString = "This is";
+  let SecondString = `${FirstString} the Main Paragraph.`;
+  MainParagraph.textContent = SecondString;
+  let Article = document.createElement("article");
+  let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3"> This is my article paragraph</p>`;
+  Article.setAttribute("class", "container")
+  Article.innerHTML = ArticleParagraph;
+  let DocumentBody;
+  DocumentBody.appendChild(Article);
 })();
 
 function DisplayHomepage() {
